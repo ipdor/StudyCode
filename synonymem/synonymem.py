55e6edb4334ec302c8dac95b5067716305ca5bc0
fanyi.lu <fanyi.lu@kingstarfintech.com>
2023-02-09 20:11:46 +0800

    增加随机模式、顺序序号、单词文件名配置

--- synonymem/synonymem.py
+++ synonymem/synonymem.py
@@ -1,33 +1,107 @@
 # coding=gb2312
 import random
+import atexit
 import re
 
-def read_config(file_name):
-    config = {}
-    with open(file_name,encoding='utf-8') as f:
-        for line in f:
-            words = line.strip().split(',')
-            config[words[0]] = set(words)
-    return config
+class Config:
+    def __init__(self,file_path):
+        self.file_path = file_path
+        self.__configs = {}
+        self.words_file = "word.txt"
+        self.random_mode = True
+        self.seq_num = 1
+
+    def read_config(self):
+        with open(self.file_path, 'r',encoding='gb2312') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+
+                parts = line.split("#")
+                key_value = parts[0].strip()
+                if "=" not in key_value:
+                    continue
+                config, parameter = key_value.split('=')
+                if parameter.strip()!='':
+                    self.__configs[config.strip()] = parameter.strip()
+
+        if "WORDSFILE" in self.__configs:
+            self.words_file = self.__configs["WORDSFILE"] 
+        if "RANDOMMODE" in self.__configs:
+            self.random_mode = bool(int(self.__configs["RANDOMMODE"]))
+        if "SEQNUM" in self.__configs:
+            self.seq_num = int(self.__configs["SEQNUM"])
+
+config = Config("config.ini")
+
+
+@atexit.register
+def clean():
+    global config
+    newfile=""
+    with open(config.file_path, 'r+',encoding='gb2312') as f:
+        #定位到那一行，然后修改配置
+        modified = False
+        while True:
+            line = f.readline()
+            if not line: 
+                if modified:
+                    break
+                else:
+                    newfile += ("SEQNUM="+str(config.seq_num)+"\n")
+                    break
+
+            if line.find("SEQNUM")>=0:
+                match = re.search("SEQNUM=\s*([0-9]+)\s*",line)
+                newfile += (line.replace(match.group(1),str(config.seq_num))) if match else ("SEQNUM="+str(config.seq_num)+"\n")
+                modified =True
+            else:
+                newfile+=line
+
+    with open(config.file_path, 'w+',encoding='gb2312') as f:
+        f.write(newfile)
+
+def read_words(file_name):
+    words = []
+    try:
+        with open(file_name,encoding='utf-8') as f:
+            for line in f:
+                lineWords = line.strip().split(',')
+                words.append(set(lineWords))
+    except Exception as e:
+        print("打开{0}文件失败".format(file_name))
+        return None
+    return words
+
+
+def memorize(words):
+    answer_group = random.choice(words) if config.random_mode else words[config.seq_num-1] #先选择一组作为answer_group
+    random_word = random.choice(list(answer_group))  #抽出随机的单词
+    print(random_word,"请输入它的同义词:")
+    user_input = input().strip()
+    user_words = {random_word}
+    for x in user_input.split(','):
+        user_words.add(x.strip()) 
+    if user_words == answer_group: #答案组和随机组比较user_words == random_group
+        print("答案正确")
+    else:
+        chr = ","
+        print("错误，正确答案是:",chr.join(list(answer_group)))
+    print("**************************************")
+    if not config.random_mode:
+        config.seq_num+=1
 
 def main():
-    config = read_config('config.ini')
+    global config
+    words = read_words(config.words_file)
+    if None == words:
+        return
     while True:
-        random_group = config[random.choice(list(config))] #先随机选择一组,也是answer_group
-        random_word = random.choice(list(random_group))  #然后从随机组中抽出随机的单词
-        print(random_word,"请输入它的同义词:")
-        user_input = input().strip()
-        user_words = {random_word}
-        for x in user_input.split(','):
-            user_words.add(x.strip()) 
-        if user_words == random_group: #答案组和随机组比较user_words == random_group
-            print("答案正确")
-        else:
-            chr = ","
-            print("错误，正确答案是:",chr.join(list(random_group)))
-        print("**************************************")
+        memorize(words)
 
 
 if __name__ == '__main__':
-    #test()
+    config.read_config()
     main()
